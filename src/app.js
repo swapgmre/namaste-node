d05b5c45c00a542813691f0b695c37f95c091405
Swapnil Gamre <swapgmre@gmail.com>
2024-10-02 16:27:00 +0530

    routing parameters

--- src/app.js
+++ src/app.js
@@ -2,24 +2,12 @@ const express = require("express");
 
 const app = express();
 
-// this will only handle GET call to /user
-app.get("/user", (req, res) => {
+
+app.get("/user/:userId", (req, res) => {
+  console.log(req.params);
   res.send({ firstName: "Swapnil", lastName: "Gamre" });
 });
 
-app.post("/user", (req, res) => {
-  //Savin data to DB
-  res.send("Data successfully saved to the Database");
-});
-
-app.delete("/user", (req, res) => {
-  res.send("User successfully deleted");
-})
-
-//this will match all the HTTP method API calls to /test
-app.use("/test", (req, res) => {
-  res.send("Hello from the Server!");
-});
 
 app.listen(7777, () => {
   console.log("Server is successfully listening on port 7777...");
