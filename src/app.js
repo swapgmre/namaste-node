dc10a7d5855407449dfd4c2f4e66771b71ce9b09
Swapnil Gamre <swapgmre@gmail.com>
2024-10-03 21:36:50 +0530

    dynamic data in signup api

--- src/app.js
+++ src/app.js
@@ -3,14 +3,11 @@ const connectDB = require("./config/database");
 const app = express();
 const User = require("./models/user");
 
+app.use(express.json());
 
 app.post("/signup", async (req, res) => {
-  const user = new User({
-    firstName: "Swapnil",
-    lastName: "Gamre",
-    emailId: "swapnil@gmail.com",
-    password: "Jnndnn123",
-  });
+
+  const user = new User(req.body);
 
   try {
     await user.save();
