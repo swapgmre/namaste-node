e05e9d888c09859717ddb9a2a0b7a758cd6bd6d4
Swapnil Gamre <swapgmre@gmail.com>
2024-10-03 15:38:19 +0530

    Error Handling in JS

--- src/app.js
+++ src/app.js
@@ -2,23 +2,22 @@ const express = require("express");
 
 const app = express();
 
-const { adminAuth, userAuth } = require("./middlewares/auth")
-
-// Handling Auth Middleware for all GET, POST, ....requets
-app.use("/admin", adminAuth);
-
-app.get("/user", userAuth, (req, res) => {
+app.get("/getUserData", (req, res) => {
+  // try {
+  // Logic of DB call and get user data
+  // throw new Error("nfindsmd");
   res.send("User Data Sent");
-})
-
-app.get("/admin/getAllData", (req, res) => {
-  res.send("All Data Sent");
+  // } catch (error) {
+  res.status(500).send("Some error contact support team");
+  // }
 });
 
-app.get("/admin/deleteUser", (req, res) => {
-  res.send("Deleted a user");
+app.use("/", (err, req, res, next) => {
+  if (err) {
+    // Log your err
+    res.status(500).send("Sonmething went wrong")
+  }
 });
-
 
 app.listen(7777, () => {
   console.log("Server is successfully listening on port 7777...");
